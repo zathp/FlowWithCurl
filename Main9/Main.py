3d351e7ea1838d7cd2cb0488d4d7481c4667c2aa
zathp <56136084+zathp@users.noreply.github.com>
2025-12-13 18:37:19 -0500

    Update Main.py

--- Main9/Main.py
+++ Main9/Main.py
@@ -2,19 +2,28 @@
 from WorldStep import WorldStep
 from Vizualizer_haze_with_topdown import run_viewer
 
+# k1 in grid kernel for diffusion and curl calculation
+# k2 in grid kernel for divergence and gradient calculation
+# k3 in grid kernel for density field diffusion
+# k4 in grid kernel for double gradient calculation
+
 
 def main():
     # Create simulation (you can tweak grid size here)
     sim = WorldStep(
-        nx=10,
-        ny=10,
+        nx=100,
+        ny=100,
         nz=10,
         lx=1.0,
         ly=1.0,
         lz=1.0,
         seed=0,
         dispersion=0.1,
-        particle_mass=20.0
+        particle_mass=20.0,
+        k1_size=9,
+        k2_size=4,
+        k3_size=5,
+        k4_size=3,
     )
 
     # Launch viewer
